1ced05653942d2d56051628db492a65bfea292a0
Randil Fernando <randil.fernando@outlook.com>
2020-04-15 23:11:40 +0530

    added database script

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
-CREATE TABLE if not exists responses (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  intent TEXT UNIQUE NOT NULL,
-  response TEXT NOT NULL
+CREATE TABLE public.response
+(
+  id       serial PRIMARY KEY NOT NULL,
+  intent   TEXT               NOT NULL,
+  response TEXT
 );
+CREATE UNIQUE INDEX response_id_uindex
+  ON public.response (id);
+CREATE UNIQUE INDEX response_intent_uindex
+  ON public.response (intent);
